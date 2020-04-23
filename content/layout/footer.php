7a3645fde4e6db9dbe596690b2701901fca10497
Goodman Luphondo <luphondog@icloud.com>
2020-04-23 19:32:30 +0200

    Touching up on Layout

--- content/layout/footer.php
+++ content/layout/footer.php
@@ -1,3 +1,54 @@
+    <footer>
+        <div class="container">
+            <img class="footer-logo" src="./images/premiumbeats-logo-black-01.png" alt="logo" width="150px">
+            <ul class="social-icons">
+                <li class="social-icon">
+                    <a href="#">
+                        <span class="fa-stack fa-2x">
+                            <i class="far fa-circle fa-stack-2x"></i>
+                            <i class="fab fa-twitter fa-stack-1x"></i>
+                        </span>
+                    </a>
+                </li>
+                <li class="social-icon">
+                    <a href="#">
+                        <span class="fa-stack fa-2x">
+                            <i class="far fa-circle fa-stack-2x"></i>
+                            <i class="fab fa-facebook-f fa-stack-1x"></i>
+                        </span></a>
+                </li>
+                <li class="social-icon">
+                    <a href="#">
+                        <span class="fa-stack fa-2x">
+                            <i class="far fa-circle fa-stack-2x"></i>
+                            <i class="fab fa-instagram fa-stack-1x"></i>
+                        </span>
+                    </a>
+                </li>
+                <li class="social-icon">
+                    <a href="#">
+                        <span class="fa-stack fa-2x">
+                            <i class="far fa-circle fa-stack-2x"></i>
+                            <i class="fab fa-soundcloud fa-stack-1x"></i>
+                        </span>
+                    </a>
+                </li>
+                <li class="social-icon">
+                    <a href="#">
+                        <span class="fa-stack fa-2x">
+                            <i class="far fa-circle fa-stack-2x"></i>
+                            <i class="fas fa-at fa-stack-1x"></i>
+                        </span>
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+        <h4>
+            © 2019 copyright all right reserved by thebigzill
+        </h4>
+        <h4>Copyright &copy; <?php echo(date("Y")); ?>. All rights reserved.</h4>
+    </footer>
 </div>
 </body>
 </html>
--- content/layout/header.php
+++ content/layout/header.php
@@ -8,6 +8,50 @@
 <html lang="en-ZA">
 <head>
 <title><?php echo(SITE_NAME); ?></title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Navbar 1</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://kit.fontawesome.com/bbdbc1be2c.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 <div id="">
+    <header>
+        <div class="container">
+            <nav>
+                <a href="index.html" class="navbar-brand">
+                    <img src="./images/premiumbeats-logo-color-01.png" alt="" width="150px">
+                </a>
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Premium Beats</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Voice Over</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Bookings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link login">Login</a>
+                    </li>
+                </ul>
+                <div class="hamburger">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+            </nav>
+        </div>
+    </header>
