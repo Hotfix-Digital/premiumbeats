9c57d3ed2e0050db97ea305844787a1cf9c834cc
Prince <princeradebe@gmail.com>
2020-05-17 13:27:42 +0200

    Add Slider

--- content/layout/index.php
+++ content/layout/index.php
@@ -50,13 +50,15 @@ get_header();
     </div> <!-- end of .slide__show -->
     <div class="container">
         <div class="soundcloud">
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/741154570&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-            <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+            <iframe scrolling="no" id="hearthis_at_track_4433241" width="100%" height="150" src="https://app.hearthis.at/embed/4433241/transparent_black/?hcolor=&color=&style=2&block_size=1&block_space=0&background=1&waveform=0&cover=0&autoplay=0&css=" frameborder="0" allowtransparency allow="autoplay">
+                <p>Listen to <a href="https://hearthis.at/premiumbeats/pb016-feat.-anerah-yasole-2nd-edit/" target="_blank">PB016 FEAT. ANERAH YASOLE - 2nd Edit</a> <span>by</span><a href="https://hearthis.at/premiumbeats/" target="_blank">Thebigzill Onair</a> <span>on</span> <a href="https://hearthis.at/" target="_blank">hearthis.at</a></p>
+            </iframe>
+            <!-- <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
                 <a href="https://soundcloud.com/premiumbeatssa" title="Thebigzill Onair" target="_blank" style="color: #cccccc; text-decoration: none;">Thebigzill Onair
                 </a> ·
                 <a href="https://soundcloud.com/premiumbeatssa/premium-beats-014-feat-dj-kts-tbm" title="Premium Beats 014 Feat. DJ KTS TBM" target="_blank" style="color: #cccccc; text-decoration: none;">Premium Beats 014 Feat. DJ KTS TBM
                 </a>
-            </div>
+            </div> -->
         </div>
         <!--.soundcloud-->
         <div class="subscribe">
@@ -78,12 +80,14 @@ get_header();
                         <span class="day">26</span>
                         <span class="month">May</span>
                         <span class="year">2020</span>
-                    </div><!--.date-->
+                    </div>
+                    <!--.date-->
                     <ul class="highlight__icons">
                         <li><a href="#" class="fa fa-bookmark-o"></a></li>
                         <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
                         <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
-                    </ul><!--Add icons for article-->
+                    </ul>
+                    <!--Add icons for article-->
                 </div>
                 <!--.highlight__header-->
                 <div class="highlight__body">
@@ -104,12 +108,14 @@ get_header();
                         <span class="day">26</span>
                         <span class="month">May</span>
                         <span class="year">2020</span>
-                    </div><!--.date-->
+                    </div>
+                    <!--.date-->
                     <ul class="highlight__icons">
                         <li><a href="#" class="fa fa-bookmark-o"></a></li>
                         <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
                         <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
-                    </ul><!--Add icons for article-->
+                    </ul>
+                    <!--Add icons for article-->
                 </div>
                 <!--.highlight__header-->
                 <div class="highlight__body">
@@ -130,12 +136,14 @@ get_header();
                         <span class="day">26</span>
                         <span class="month">May</span>
                         <span class="year">2020</span>
-                    </div><!--.date-->
+                    </div>
+                    <!--.date-->
                     <ul class="highlight__icons">
                         <li><a href="#" class="fa fa-bookmark-o"></a></li>
                         <li><a href="#" class="fa fa-heart-o"><span>18</span></a></li>
                         <li><a href="#" class="fa fa-comment-o"><span>3</span></a></li>
-                    </ul><!--Add icons for article-->
+                    </ul>
+                    <!--Add icons for article-->
                 </div>
                 <!--.highlight__header-->
                 <div class="highlight__body">
@@ -150,7 +158,8 @@ get_header();
                 <!--.highlight__body-->
             </div>
             <!--.news__highlight-->
-        </div> <!--.news__highlights card-->
+        </div>
+        <!--.news__highlights card-->
     </div><!-- end of .container -->
 </div>
 <!--- .content -->
