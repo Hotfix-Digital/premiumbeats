5a41fd14a4f2f56e9f844e78f54bbbcd7ebd7e3b
princeradebe <princeradebe@gmail.com>
2020-06-25 23:13:52 +0200

    Add tracklist, still to make it mobile responsive

--- content/layout/mixes.php
+++ content/layout/mixes.php
@@ -15,6 +15,40 @@ get_header();
                 <button class="tracklisting">
                     Tracklist
                 </button>
+                <div class="tracklist__body">
+                    <div class="tracklist__content">
+                        <h2 class="mix__title">Premium Beats 017</h2>
+                        <h2>1st Hour Mixed by Thebigzill Onair</h2>
+                        <ol class="mix__hour">
+                            <li>SBCNCSLY - Black Coffee & Sabrina Claudio</li>
+                            <li>Mizimu - Echo Deep</li>
+                            <li>Garden - &ME</li>
+                            <li>I Miss the Things (FKA Mash Re-Glitch - Lars Behrenroth & Sinan Baymak</li>
+                            <li>La Danse - Claude Monnet</li>
+                            <li>There Is Still Pain Left (Laolu Remix) - Sophie Hunger</li>
+                            <li>Whistleman (Pablo Fierro Remix) - Bedouin</li>
+                            <li>Zama Kuwe - Dee Cee</li>
+                            <li>Just Let Go (feat. Zemira Israel) - Birds of Mind</li>
+                            <li>It's Only (DJ Koze Mix - Bonus Track) - Matthew Herbert</li>
+                            <li>Dream (Enoo Napa Remix) - Literatura</li>
+                            <li>Cobra XL (Ed-Ward Remix) - Martin Iveson</li>
+                            <li>Walking Away - Mind Against</li>
+                        </ol>
+                        <h2 >2nd Hour Mixed by Theocratous</h2>
+                        <ol class="mix__hour">
+                            <li>Ronald Clark - Resist ( Accapella )</li>
+                            <li>Lane_8 & Anderholm - bluebird </li>
+                            <li>Zakes Bantwini  - Amanga</li>
+                            <li>Literatura - Dream ( Enoo Napa Remix )</li>
+                            <li>Kusasa - Coas ( ( Original Mix )</li>
+                            <li>Pex Africa - Mdali wethu ( Dj Micks Remix )</li>
+                            <li>Dj Whisky - Boni Mores ( Club Mix )</li>
+                            <li>Benny T - Dark Elephants ( Original mix )</li>
+                            <li>Claudio X Kenza - Bass Of Africa</li>
+                            <li>Samurai Yasusa & Soulholic - Noma’s Theme</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
             <div class="splash__container mix__16">
                 <div class="container mix__player">
@@ -25,6 +59,40 @@ get_header();
                 <button class="tracklisting">
                     Tracklist
                 </button>
+                <div class="tracklist__body">
+                    <div class="tracklist__content">
+                    <h2 class="mix__title">Premium Beats 016</h2>
+                        <h2>1st Hour Mixed by Thebigzill Onair</h2>
+                        <ol class="mix__hour">
+                            <li>Mamey (feat. Y.O.G.A.) - Djuma Soundsystem & Demian Muller</li>
+                            <li>Drained feat. Sooma - David Mayer</li>
+                            <li>Journey (Extended Mix) - Black Circle</li>
+                            <li>Ka Sarraysa (Original mix) - Gratias Deo</li>
+                            <li>Zolalela (feat. Lizwi) [Sebastien Dutch vs Kreative Nativez Remix] - Black Major</li>
+                            <li>Levan - &lez</li>
+                            <li>Leave Us (feat. Lazarusman) [Kususa Remix] - Djuma Soundsystem & Pete Oak</li>
+                            <li>Duende (Karyendasoul Remix) - Djuma Soundsystem & Bongo & Pusk</li>
+                            <li>Hide U (Chicola Extended Remix) - Sandy Rivera & Rae</li>
+                            <li>Phula (Kususa & Argento Dust Remix) - Amanda Mo feat. DJ Maphorisa</li>
+                            <li>Kali Koi - Drega</li>
+                            <li>Nasie ( Orignal mix ) - DJ Kaizer</li>
+                            <li>Better Days - Themba</li>
+                            <li>Bora Bora - Drega</li>
+                        </ol>
+                        <h2 >2nd Hour Mixed by Anerah Yasole</h2>
+                        <ol class="mix__hour">
+                            <li>Kollektiv Turmstrasse - Ribbon Reef</li>
+                            <li>SpheraQ- Kelemeo</li>
+                            <li>Marino Canal - Dreamt of Tomorrow</li>
+                            <li>Kaz James - Through Your Love (feat. Mr.id & Kawtar Sadik) (Guy Gerber Remix) (Extended)</li>
+                            <li>Boot Slap feat. Chiara Montiel - Cocodrilos</li>
+                            <li>Fake Mood feat. Idd AzizJowa (Original Mix)</li>
+                            <li>Kevin Saunderson, E-Dancer feat. Virus J - Oombah (THEMBA's Herd Extended Remix)</li>
+                            <li>De Cave Man & TonicVolts - You Were Here (&lez Remix)</li>
+                            <li>Bryan Kessler - Cucú</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
             <!--- .splash__container -->
             <div class="splash__container mix__15">
@@ -36,6 +104,41 @@ get_header();
                 <button class="tracklisting">
                     Tracklist
                 </button>
+                <div class="tracklist__body">
+                    <div class="tracklist__content">
+                    <h2 class="mix__title">Premium Beats 015</h2>
+                        <h2>1st Hour Mixed by Thebigzill Onair</h2>
+                        <ol class="mix__hour">
+                            <li>Afro Sambroso (Sequ3l Remix) - Lokkhi Terra & Dele Sosimi</li>
+                            <li>Possibilities - Karyendasoul</li>
+                            <li>Temple of Snakes (feat. Martin Iveson) - Jullian Gomes</li>
+                            <li>Why Does My Heart Feel So Bad (Oxia Dub Mix) - Moby</li>
+                            <li>No Dancers (Adam Port Extended Mix) - Cubicolor</li>
+                            <li>Love Will Wait (Kususa Remix Instrumental) - Jackie Queens</li>
+                            <li>Its Over (Eltonnick Remix) [feat. Kafele] - Rocco Rodamaal</li>
+                            <li>Incwadi Encane - Kususa & Argento Dust</li>
+                            <li>Density - Manyelo Dafro</li>
+                            <li>Hell to the Liars (Kölsch Remix) - London Grammar</li>
+                            <li>Birdsong (feat. Zola Blood) [Super Flu Remix] - Claptone</li>
+                            <li>Endangered Audio Research - Johannes Brecht</li>
+                            <li>Working Hard (feat. Akram) [Enoo Napa Remix] - Rocco Rodamaal</li>
+                        </ol>
+                        <h2 >2nd Hour Mixed by DJ NOTRIX</h2>
+                        <ol class="mix__hour">
+                            <li>Jazzuelle - Eclipse (Darkside Dub)</li>
+                            <li>Ceebar & De'Keay - Issues (Aqua Dub Mix)</li>
+                            <li>The Godfathers of Deep House - Get The Groove (Nostalgic Mix)</li>
+                            <li>Buddynice & Ceebar - The Plug (Redimial Afrotech)</li>
+                            <li>Dj Notrix - Aye (Original Mix)</li>
+                            <li>De Khoisan Afrikah - The Khoisan Dance</li>
+                            <li>Afro Victimz & Vida Soul - Moving Train (Afro House mix)</li>
+                            <li>Budda Sage & Tee-R - Idlozi (Ed-Ward Remix)</li>
+                            <li>Vida-Soul & Dj Jim Mastershine - Wrong Bike (Original mix)</li>
+                            <li>Samurai Yasusa & Soulholic - Noma’s Theme</li>
+                            <li>Aimo - The Verdict (Original Mix)</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
             <!--- .splash__container -->
         </div>
